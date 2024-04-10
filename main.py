2ab8679c7f068ed15153a69df0c1800e1ff87895
Jesse83005 <166648893+Jesse83005@users.noreply.github.com>
2024-04-10 15:42:42 -0400

    Added decode function  -Jesse

--- main.py
+++ main.py
@@ -6,23 +6,42 @@ def encode(password):
         encoded_password += str(encoded_digit)
     return encoded_password
 
+def decode(encoded_password):
+    numlist = list(str(encoded_password))
+    intlist = []
+    final = []
+    for i in numlist:
+        x = int(i)
+        intlist.append(x)
+    for i in intlist:
+        x = i - 3
+        final.append(x)
+    return ("".join(str(num) for num in final))
 
-if __name__ == '__main__':
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
 
-    option = int(input("Please enter an option: "))
-    password = str(input("Please enter your password to encode: "))
 
-    if option == 1:
-        encoded_password = encode(password)
-        print("Your password has been encoded and stored!")
+encoded_password = None
+while True:
+    if __name__ == '__main__':
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
 
-    elif option == 2:
-        print(f"The encoded password is {}, and the original password is{}.")
+        option = int(input("Please enter an option: "))
 
-    elif option == 3:
-        exit()
+        if option == 1:
+            password = str(input("Please enter your password to encode: "))
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif option == 2:
+            if encoded_password is not None:
+                print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.\n")
+            else:
+                print("There is no password to be decoded\n")
+        elif option == 3:
+            break
+        else:
+            print("Invalid option, please try again\n")
